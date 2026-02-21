159e6f2acb395a8f809c949a3aeae4a434fb1d70
niyogibertos@gmail.com <39372021+GilbertFromRwanda@users.noreply.github.com>
2026-02-21 11:54:11 +0200

    refactor: update database configuration and model field types for PostgreSQL compatibility (#1)
    Co-authored-by: Ndizihiwe Benny Chrispin <ndizibaidu23@gmail.com>

--- database/database.go
+++ database/database.go
@@ -6,18 +6,18 @@ import (
 
 	"nfs-api/config"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func Connect(cfg *config.Config) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName,
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+		cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort,
 	)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
